fe7f1c24cca03ee468bde8c7aada6bb374074e65
mgupta701 <mallika.gupta104@gmail.com>
2022-08-29 11:45:27 -0700

    Adjusting timeseries colors
    Editing depth range colors, cleaning up timeseries_draft.R file

--- scripts/drafts/timeseries_draft.R
+++ scripts/drafts/timeseries_draft.R
@@ -6,6 +6,9 @@ bottle$station <- as.numeric(bottle$station)
 
 ###### Time series plot for oxygen
 
+n_ranges <- 3
+percentile <- 5
+
 # Modifying original ts plot to show percentile
 oxy_ts_plot_percentile <- function(n_ranges, percentile, date_min, date_max){
   if (n_ranges > 1){
@@ -51,60 +54,20 @@ oxy_ts_plot_percentile <- function(n_ranges, percentile, date_min, date_max){
                  breaks = scales::breaks_pretty(7)) +
     scale_y_continuous(limits=c(NA,NA), 
                        expand = c(0.1, 0.1)) +
+    scale_color_manual(values=c("#6BAED6", "#2171B5", "#08306B")) + 
     theme_bw() 
   
 }
+
+brewer.pal(n = 9, name = "Blues")  # blue color palette
 
 oxy_ts_plot_percentile(3, 5, '1949-02-28', '2020-01-26')
 
 
-# Time series plot of the standard deviation in oxygen values
-oxy_ts_plot_sd <- function(n_ranges, date_min, date_max){
-  if (n_ranges > 1){
-    bottle_sub <- bottle %>% 
-      subset(station <= 60) %>%
-      filter(depth>=0 & depth<=500) %>%
-      mutate(depth_fac = cut(depth, n_ranges))
-  }
-  else {
-    bottle_sub <- bottle %>% 
-      subset(station <= 60) %>%
-      filter(depth>=0 & depth<=500) %>%
-      mutate(depth_fac = rep("[0,500]", length(year)))
-  }
-  
-  z <- bottle_sub %>%
-    group_by(year, quarter, depth_fac) %>%
-    summarise(oxygen_sd = sd(oxygen, na.rm = TRUE),
-              date = median(date, na.rm = T))
-  
-  z %>% 
-    ggplot(aes(x = date, 
-               y = oxygen_sd, 
-               group = depth_fac, 
-               color = depth_fac, 
-               shape = as.factor(quarter))) +
-    geom_point(na.rm=T) +
-    geom_line(linetype='dashed') +
-    labs(title = "Standard Deviation in Oxygen Across All On-Shelf Stations Over Time", 
-         x = "Date", 
-         y = "Oxygen (mL/L)", 
-         color = "Depth Range (m)", 
-         shape = "Quarter") +
-    scale_shape_discrete(name="Quarter",
-                         breaks=c("1", "2", "3","4"),
-                         labels=c("Winter", "Spring", "Summer","Fall")) +
-    scale_x_date(limit = c(as.Date(date_min), as.Date(date_max)), 
-                 date_labels = "%Y %b %d", 
-                 breaks = scales::breaks_pretty(7)) +
-    scale_y_continuous(limits=c(NA,NA), 
-                       expand = c(0.1, 0.1)) +
-    theme_bw() 
-  
-}
-
-oxy_ts_plot_sd(5, '1970-06-14', '2020-01-26')
-
+bottle_74 <- bottle_sub %>%
+  filter(year == 1974)
+hist(bottle_sub$depth)
+hist(bottle_74$depth)
 
 ## Faceting by  quarter
 
